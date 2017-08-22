26d743acab0600a311599d1b205859a10abed8c8
Dylan <dylan.ebert@gmail.com>
2017-08-21 21:22:08 -0400

    ..

--- Assets/Scripts/UI/MouseOverButton.cs
+++ Assets/Scripts/UI/MouseOverButton.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class MouseOverButton : MonoBehaviour {
 
     public virtual void MouseOver() {
-        transform.localScale = Vector3.one * 1.25f;
+        transform.localScale = Vector3.one * 1.2f;
     }
 
     public virtual void MouseExit() {
